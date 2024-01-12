86c42cf4f8e07a02b0c998a479d5a5601de05dd2
Simone De Gennaro <simone.degennaro@yahoo.it>
2024-01-12 16:41:33 +0100

    Fix new Legabasket boxscore

--- src/util/boxscore.ts
+++ src/util/boxscore.ts
@@ -1,4 +1,3 @@
-import * as jsdom from "jsdom";
 import * as fetch from "node-fetch";
 import { IPerformance } from "../schemas/performance";
 import { IPlayer } from "../schemas/player";
@@ -11,19 +10,74 @@ interface BoxScore {
   plusMinus?: number;
 }
 
-export const boxscore = async (performances: IPerformance[], url: string) => {
-  const response = await fetch.default(url);
-  let content = await response.text();
-  const dom = new jsdom.JSDOM(content);
-  const ht_match_scores: HTMLCollection = dom.window.document
-    .getElementById("ht_match_scores")
-    ?.querySelector("tbody")?.children as HTMLCollection;
-  const vt_match_scores: HTMLCollection = dom.window.document
-    .getElementById("vt_match_scores")
-    ?.querySelector("tbody")?.children as HTMLCollection;
+interface LegabasketBoxscore {
+  scores: Scores;
+}
 
-  const tabellinoCasa = calcolaBoxScore(ht_match_scores);
-  const tabellinoTrasferta = calcolaBoxScore(vt_match_scores);
+interface Scores {
+  ht: LegabasketPerformances;
+  vt: LegabasketPerformances;
+}
+
+export interface LegabasketPerformances {
+  rows: LegabasketPerformance[];
+}
+
+export interface LegabasketPerformance {
+  player_id?: number;
+  player_num?: number;
+  player_surname?: string;
+  player_name?: string;
+  player_p_key?: null | string;
+  pun: number;
+  min: number;
+  sf?: string;
+  falli_c: number;
+  falli_sf: number;
+  t2_r: number;
+  t2_t: number;
+  t2_perc: number;
+  sc: number;
+  t3_r: number;
+  t3_t: number;
+  t3_perc: number;
+  tl_r: number;
+  tl_t: number;
+  tl_perc: number;
+  rimbalzi_o: number;
+  rimbalzi_d: number;
+  rimbalzi_t: number;
+  stoppate_dat: number;
+  stoppate_sub: number;
+  palle_p: number;
+  palle_r: number;
+  ass: number;
+  val_lega: number;
+  val_oer: number;
+  plus_minus?: number;
+}
+
+export const boxscore = async (performances: IPerformance[], url: string) => {
+  const tokenResponse = await fetch.default(
+    "https://www.legabasket.it/api/oauth"
+  );
+  const tokenValue = await tokenResponse.text();
+  const tokenObj = JSON.parse(tokenValue);
+  const token = tokenObj.data.token;
+  const gameId = url.split("/")[4];
+  const headers = new fetch.Headers();
+  headers.set("Authorization", `Bearer ${token}`);
+
+  const response = await fetch.default(
+    `https://api-lba.procne.cloud/api/v1/championships_matches/${gameId}/scores`,
+    { headers }
+  );
+  const boxscoreJSON = await response.text();
+  const boxscore: LegabasketBoxscore = JSON.parse(boxscoreJSON);
+  const ht = boxscore.scores.ht.rows as LegabasketPerformance[];
+  const vt = boxscore.scores.vt.rows as LegabasketPerformance[];
+  const tabellinoCasa = calcolaBoxScore(ht);
+  const tabellinoTrasferta = calcolaBoxScore(vt);
 
   for (const performance of performances) {
     await performance.populate("player");
@@ -52,39 +106,19 @@ export const boxscore = async (performances: IPerformance[], url: string) => {
   }
 };
 
-function calcolaBoxScore(ht_match_scores: HTMLCollection): BoxScore[] {
-  const length = ht_match_scores.length;
-  console.log(length);
-  const tabellinoCasa: BoxScore[] = [];
-  for (let i = 0; i < length; i++) {
-    const tableDatas = ht_match_scores[i]?.children;
-    const tdLength = tableDatas.length;
+function calcolaBoxScore(performances: LegabasketPerformance[]): BoxScore[] {
+  const tabellino: BoxScore[] = [];
+
+  performances.forEach((player) => {
     const boxScore: BoxScore = {};
-    for (let j = 0; j < tdLength; j++) {
-      const td: HTMLTableCellElement = tableDatas[j] as HTMLTableCellElement;
-      if (i < length - 2) {
-        // solo i giocatori: Squadra e totali li ignoriamo
-        if (j == 0) {
-          const surname = td.getElementsByClassName("scores_player_surname")[0]
-            ?.textContent;
-          const name = td
-            .getElementsByClassName("scores_player_name")[0]
-            ?.textContent?.toUpperCase();
-          boxScore.name = `${name} ${surname}`;
-        } else if (j === 2) {
-          boxScore.minutes = Number(td?.textContent);
-        } else if (j == 24) {
-          boxScore.ranking = Number(td?.textContent);
-        } else if (j == 25) {
-          boxScore.oer = Number(td?.textContent?.replace(",", "."));
-        } else if (j == 26) {
-          boxScore.plusMinus = Number(td?.textContent);
-        }
-      }
-    }
-    tabellinoCasa.push(boxScore);
-  }
-  return tabellinoCasa;
+    boxScore.name = `${player.player_name!.toUpperCase()} ${player.player_surname!.toUpperCase()}`;
+    boxScore.minutes = player.min;
+    boxScore.ranking = player.val_lega;
+    boxScore.oer = player.val_oer;
+    boxScore.plusMinus = player.plus_minus;
+    tabellino.push(boxScore);
+  });
+  return tabellino;
 }
 
 function isNumeric(str: any) {
